3a1d0cfd63c3333f409e0b3c93c8e64eeccd2bc0
jgadbois17 <josephgadbois17@gmail.com>
2021-04-20 14:30:23 -0700

    add ch1 r codes

---
+++ chapter_codes/1_5_Kolmogorov_Smirnov_Test.R
@@ -0,0 +1,49 @@
+#--------------------------------------------------------------------------------
+
+# Ch 1: Hypothesis Testing for Two Samples
+# S5: Kolmogorov-Smirnov Test for Equality of Distributions
+
+# Joey Gadbois
+
+#--------------------------------------------------------------------------------
+
+# Example 1.13
+
+# data
+bioimplant = list(
+  Tx = c(3, 4, 6, 6, 8, 8), 
+  Cx = c(4, 4, 4, 5, 6, 7, 8, 10, 12)
+)
+
+# kolmogorov-smirnov test
+ks.test(bioimplant$Tx, bioimplant$Cx, alternative = 'greater')
+
+#--------------------------------------------------------------------------------
+
+# Example 1.14
+
+# data
+leukemia = list(
+  Tx = c(12.33, 10.44, 12.72, 13.13, 13.50, 16.82, 17.60, 14.37), 
+  Cx = c(16.45, 18.63, 13.12, 18.94, 19.34, 22.50, 21.56, 17.63)
+)
+
+# kolmogorov-smirnov test
+ks.test(leukemia$Cx, leukemia$Tx, alternative = 'less')
+
+#--------------------------------------------------------------------------------
+
+# Example 1.15
+
+# data
+elderly = list(
+  men = c(4, 5, 4, 3, 4, 3), 
+  women = c(5, 5, 4, 4, 4, 5, 4)
+)
+
+# kolmogorov-smirnov test
+ks.test(elderly$men, elderly$women, alternative = 'two.sided')
+
+#--------------------------------------------------------------------------------
+
+
